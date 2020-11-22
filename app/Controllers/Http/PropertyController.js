99322193926fa3c72b06127e5938bdfeb35f8e36
Henrique Lopes <henrique.henriquelopess@gmail.com>
2020-11-22 15:34:51 -0300

    feat: Structuring Property and Image entities

---
+++ app/Controllers/Http/PropertyController.js
@@ -0,0 +1,21 @@
+'use strict'
+
+class PropertyController {
+
+  async index ({ request, response, view }) {
+  }
+
+  async store ({ request, response }) {
+  }
+
+  async show ({ params, request, response, view }) {
+  }
+
+  async update ({ params, request, response }) {
+  }
+
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = PropertyController
